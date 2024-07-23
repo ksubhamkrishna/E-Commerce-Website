b7a88023d9f85f00a4adf7c66932ff740af93571
Subham Krishna <subham.krishna@girmiti.in>
2024-07-23 19:06:42 +0530

    14th commit

--- 03-frontend/react-ecommerce/src/layouts/ProductCheckoutPage/CheckoutAndReviewBox.tsx
+++ 03-frontend/react-ecommerce/src/layouts/ProductCheckoutPage/CheckoutAndReviewBox.tsx
@@ -3,8 +3,25 @@ import ProductModel from "../../models/ProductModel";
 
 export const CheckoutAndReviewBox: React.FC<{
     product: ProductModel | undefined, mobile: boolean,
-    currentLoansCount: number
+    currentLoansCount: number, isAuthenticated: any, isCheckedOut: boolean,
+    checkoutProduct: any
 }> = (props) => {
+
+
+    function buttonRender() {
+        if (props.isAuthenticated) {
+            if (!props.isCheckedOut && props.currentLoansCount < 5) {
+                return (<button onClick={() => props.checkoutProduct()} className='btn btn-success btn-lg'>Checkout</button>)
+            } else if (props.isCheckedOut) {
+                return (<p><b>Product checked out. Enjoy</b></p>)
+            } else if (!props.isCheckedOut && props.currentLoansCount >= 5) {
+                return (<p className="text-danger"> Too many products checked out.</p>)
+            }
+        }
+        return (<Link to={'/login'} className="btn btn-success btn-lg">Sign in</Link>)
+    }
+
+
     return (
 
         <div className={props.mobile ? 'card d-flex mt-5' : 'card col-3 container d-flex mb-5'}>
@@ -35,7 +52,7 @@ export const CheckoutAndReviewBox: React.FC<{
                         </p>
                     </div>
                 </div>
-                <Link to='/#' className='btn btn-success btn-lg'>Sign in</Link>
+                {buttonRender()}
                 <hr />
                 <p className="mt-3">
                     This number can change until placing order has been complete.
--- 03-frontend/react-ecommerce/src/layouts/ProductCheckoutPage/ProductCheckoutPage.tsx
+++ 03-frontend/react-ecommerce/src/layouts/ProductCheckoutPage/ProductCheckoutPage.tsx
@@ -8,7 +8,6 @@ import LatestReviews from "./LatestReviews";
 import { useOktaAuth } from "@okta/okta-react";
 
 
-
 export const ProductCheckoutPage = () => {
 
     const { authState } = useOktaAuth();
@@ -26,179 +25,92 @@ export const ProductCheckoutPage = () => {
     const [currentLoansCount, setCurrentLoansCount] = useState(0);
     const [isLoadingCurrentLoansCount, setIsLoadingCurrentLoansCount] = useState(true);
 
+    // Is Book Check Out?
+    const [isCheckedOut, setIsCheckedOut] = useState(false);
+    const [isLoadingProductCheckedOut, setIsLoadingProductCheckedOut] = useState(true);
+
+
     const productId = (window.location.pathname).split('/')[2];
 
-    // useEffect(() => {
-    //     const fetchProduct = async () => {
-    //         const baseUrl: string = `http://localhost:8080/api/products/${productId}`;
+    useEffect(() => {
+        const fetchProduct = async () => {
+            const baseUrl: string = `http://localhost:8080/api/products/${productId}`;
 
-    //         const response = await fetch(baseUrl);
+            const response = await fetch(baseUrl);
 
-    //         // guard clause
-    //         if (!response.ok) {
-    //             throw new Error('Something went wrong!');
-    //         }
+            // guard clause
+            if (!response.ok) {
+                throw new Error('Something went wrong!');
+            }
 
 
-    //         const responseJson = await response.json();
+            const responseJson = await response.json();
 
-    //         const loadedProduct: ProductModel = {
-    //             id: responseJson.id,
-    //             title: responseJson.title,
-    //             seller: responseJson.seller,
-    //             description: responseJson.description,
-    //             quantities: responseJson.quantities,
-    //             quantityAvailable: responseJson.quantityAvailable,
-    //             category: responseJson.category,
-    //             img: responseJson.img,
-    //         };
+            const loadedProduct: ProductModel = {
+                id: responseJson.id,
+                title: responseJson.title,
+                seller: responseJson.seller,
+                description: responseJson.description,
+                quantities: responseJson.quantities,
+                quantityAvailable: responseJson.quantityAvailable,
+                category: responseJson.category,
+                img: responseJson.img,
+            };
 
-    //         setProduct(loadedProduct);
-    //         setIsLoading(false);
-    //     };
-    //     fetchProduct().catch((error: any) => {
-    //         setIsLoading(false);
-    //         setHttpError(error.message);
+            setProduct(loadedProduct);
+            setIsLoading(false);
+        };
+        fetchProduct().catch((error: any) => {
+            setIsLoading(false);
+            setHttpError(error.message);
 
-    //     })
+        })
 
-    // }, []);
-
-    // useEffect(() => {
-    //     const fetchProductReviews = async () => {
-    //         const reviewUrl: string = `http://localhost:8080/api/reviews/search/findByProductId?productid=${productId}`
-
-    //         const responseReviews = await fetch(reviewUrl);
-
-    //         if (!responseReviews.ok) {
-    //             throw new Error('Something went wrong');
-    //         }
-
-    //         const responseJsonReviews = await responseReviews.json();
-
-    //         const responseData = responseJsonReviews._embedded.reviews;
-
-    //         const loadedReviews: ReviewModel[] = [];
-
-    //         let weightedStarReviews: number = 0;
-
-    //         for (const key in responseData) {
-    //             loadedReviews.push({
-    //                 id: responseData[key].id,
-    //                 userEmail: responseData[key].userEmail,
-    //                 date: responseData[key].date,
-    //                 rating: responseData[key].rating,
-    //                 product_id: responseData[key].product_id,
-    //                 reviewDescription: responseData[key].reviewDescription,
-    //             });
-    //             weightedStarReviews = weightedStarReviews + responseData[key].rating;
-    //         }
-
-    //         if (loadedReviews) {
-    //             const round = (Math.round((weightedStarReviews / loadedReviews.length) * 2) / 2).toFixed(1);
-    //             setTotalStars(Number(round));
-    //         }
-
-    //         setReviews(loadedReviews);
-    //         setIsLoadingReview(false);
-
-    //     };
-
-    //     fetchProductReviews().catch((error: any) => {
-    //         setIsLoadingReview(false);
-    //         setHttpError(error.message);
-    //     })
-    // }, []);
-
-    // useEffect(() => {
-    //     const fetchUserCurrentLoansCount = async () => {
-    //         if (authState && authState.isAuthenticated){
-    //             const url = `http://localhost:8080/api/reviews/secure/user/product?productId = ${productId}`;
-    //             const requestOptions = {
-    //                 method: 'GET',
-    //                 headers:{
-    //                   Authorization:`Bearer  ${authState.accessToken?.accessToken},`
-    //                   'Content-Type' : 'application/json'
-    //                 }
-    //             };
-
-    //             const userReview = await fetch(url, requestOptions);
-    //             if (!userReview.ok) {
-    //                 throw new Error('Something went wrong');
-    //             }
-
-    //             const userReviewResponseJson = await userReview.json();
-    //             setIsReviewLeft(userReviewResponseJson);
-    //         }
-    //         setIsLoadingUserReview(false);
-    //     }
-    //     fetchUserReviewProduct().catch((error: any) => {
-    //         setIsLoadingUserReview(false);
-    //         setHttpError(error.message);
-    //     })
-    // }, [authState]);
-
-    // useEffect(() => {
-    //     const fetchUserCurrentLoansCount = async () => {
-    //         if (authState && authState.isAuthenticated) {
-    //             const url = `http://localhost:8080/api/products/secure/currentloans/count`;
-    //             const requestOptions = {
-    //                 method: 'GET',
-    //                 headers: { 
-    //                     Authorization: `Bearer ${authState.accessToken?.accessToken}`,
-    //                     'Content-Type': 'application/json'
-    //                  }
-    //             };
-    //             const currentLoansCountResponse = await fetch(url, requestOptions);
-    //             if (!currentLoansCountResponse.ok)  {
-    //                 throw new Error('Something went wrong!');
-    //             }
-    //             const currentLoansCountResponseJson = await currentLoansCountResponse.json();
-    //             setCurrentLoansCount(currentLoansCountResponseJson);
-    //         }
-    //         setIsLoadingCurrentLoansCount(false);
-    //     }
-    //     fetchUserCurrentLoansCount().catch((error: any) => {
-    //         setIsLoadingCurrentLoansCount(false);
-    //         setHttpError(error.message);
-    //     })
-    // }, [authState, isCheckedOut]);
-
-    // useEffect(() => {
-    //     const fetchUserCheckedOutProduct = async () => {
-    //         if (authState && authState.isAuthenticated) {
-    //             const url = `http://localhost:8080/api/products/secure/ischeckedout/byuser/?productId=${productId}`;
-    //             const requestOptions = {
-    //                 method: 'GET',
-    //                 headers: {
-    //                     Authorization: `Bearer ${authState.accessToken?.accessToken}`,
-    //                     'Content-Type': 'application/json'
-    //                 }
-    //             };
-    //             const bookCheckedOut = await fetch(url, requestOptions);
-
-    //             if (!bookCheckedOut.ok) {
-    //                 throw new Error('Something went wrong!');
-    //             }
-
-    //             const productCheckedOutResponseJson = await productCheckedOut.json();
-    //             setIsCheckedOut(productCheckedOutResponseJson);
-    //         }
-    //         setIsLoadingproductCheckedOut(false);
-    //     }
-    //     fetchUserCheckedOutProduct().catch((error: any) => {
-    //         setIsLoadingProductCheckedOut(false);
-    //         setHttpError(error.message);
-    //     })
-    // }, [authState]);
+    }, [isCheckedOut]);
 
     useEffect(() => {
+        const fetchProductReviews = async () => {
+            const reviewUrl: string = `http://localhost:8080/api/reviews/search/findByProductId?productid=${productId}`;
 
+            const responseReviews = await fetch(reviewUrl);
+
+            if (!responseReviews.ok) {
+                throw new Error('Something went wrong');
+            }
+
+            const responseJsonReviews = await responseReviews.json();
+
+            const responseData = responseJsonReviews._embedded.reviews;
+
+            const loadedReviews: ReviewModel[] = [];
+
+            let weightedStarReviews: number = 0;
+
+            for (const key in responseData) {
+                loadedReviews.push({
+                    id: responseData[key].id,
+                    userEmail: responseData[key].userEmail,
+                    date: responseData[key].date,
+                    rating: responseData[key].rating,
+                    product_id: responseData[key].product_id,
+                    reviewDescription: responseData[key].reviewDescription,
+                });
+                weightedStarReviews = weightedStarReviews + responseData[key].rating;
+            }
+
+            if (loadedReviews) {
+                const round = (Math.round((weightedStarReviews / loadedReviews.length) * 2) / 2).toFixed(1);
+                setTotalStars(Number(round));
+            }
+            setReviews(loadedReviews);
+            setIsLoadingReview(false);
+        };
+        fetchProductReviews().catch((error: any) => {
+            setIsLoadingReview(false);
+            setHttpError(error.message);
+        })
     }, []);
 
-    useEffect(() => {
-
-    }, []);
 
     useEffect(() => {
         const fetchUserCurrentLoansCount = async () => {
@@ -215,29 +127,46 @@ export const ProductCheckoutPage = () => {
                 if (!currentLoansCountResponse.ok) {
                     throw new Error('Something went wrong!');
                 }
-
                 const currentLoansCountResponseJson = await currentLoansCountResponse.json();
                 setCurrentLoansCount(currentLoansCountResponseJson);
-
             }
-
             setIsLoadingCurrentLoansCount(false);
         }
         fetchUserCurrentLoansCount().catch((error: any) => {
             setIsLoadingCurrentLoansCount(false);
             setHttpError(error.message);
         })
+    }, [authState, isCheckedOut]);
+    useEffect(() => {
+        const fetchUserCheckedOutProduct = async () => {
+            if (authState && authState.isAuthenticated) {
+                const url = `http://localhost:8080/api/products/secure/ischeckedout/byuser/?productId= ${productId}`;
+                const requestOptions = {
+                    method: 'GET',
+                    headers: {
+                        Authorization: `Bearer ${authState.accessToken?.accessToken}`,
+                        'Content-Type': 'application/json'
+                    }
+                };
+                const productCheckedOut = await fetch(url, requestOptions);
+                if (!productCheckedOut.ok) {
+                    throw new Error('Something went Wrong!');
+                }
+                const productCheckedOutResponseJson = await productCheckedOut.json();
+                setIsCheckedOut(productCheckedOutResponseJson);
+            }
+            setIsLoadingProductCheckedOut(false);
+        }
+        fetchUserCheckedOutProduct().catch((error: any) => {
+            setIsLoadingProductCheckedOut(false);
+            setHttpError(error.message);
+        })
     }, [authState]);
-
-
-
-    if (isLoading || isLoadingReview || isLoadingCurrentLoansCount) {
+    if (isLoading || isLoadingReview || isLoadingCurrentLoansCount || isLoadingProductCheckedOut) {
         return (
             <SpinnerLoading />
-
         )
     }
-
     if (httpError) {
         return (
             <div className="container m-5">
@@ -245,7 +174,6 @@ export const ProductCheckoutPage = () => {
             </div>
         )
     }
-
     async function checkoutProduct() {
         const url = `http://localhost:8080/api/products/secure/checkout/?productId=${product?.id}`;
         const requestOptions = {
@@ -254,63 +182,65 @@ export const ProductCheckoutPage = () => {
                 Authorization: `Bearer ${authState?.accessToken?.accessToken}`,
                 'Content-Type': 'application/json'
             }
-        }};
-        return (
-            <div>
-                <div className="container d-none d-lg-block">
-                    <div className="row mt-5">
+        };
+
+        const checkoutResponse = await fetch(url, requestOptions);
+        if (!checkoutResponse.ok) {
+            throw new Error('Something went wrong!');
+        }
+
+        setIsCheckedOut(true);
+    }
 
 
-                        <div className="col-sm-2 col-md-2">
-                            {product?.img ?
-                                <img src={product?.img} width='226' height='349' alt='Product' />
-                                :
-                                <img src={require('./../../Images/BooksImages/Fashion.jpg')} width='226' height='349' alt='Product' />
-
-                            }
-                        </div>
-                        <div className="col-4 col-md-4 container">
-                            <div className="ml-2">
-                                <h2>{product?.title}</h2>
-                                <h5 className="text-primary">{product?.seller}</h5>
-                                <p className="lead">{product?.description}</p>
-                                <StarsReview rating={totalStars} size={32} />
-                            </div>
-
-                        </div>
-                        <CheckoutAndReviewBox product={product} mobile={false} currentLoansCount={currentLoansCount} />
-
-                    </div>
-                    <hr />
-                    <LatestReviews reviews={reviews} productId={product?.id} mobile={true} />
-
-                </div>
-                <div className="container d-lg-none mt-5">
-                    <div className="d-flex justify-content-center align-items-center">
-
+    return (
+        <div>
+            <div className="container d-none d-lg-block">
+                <div className="row mt-5">
+                    <div className="col-sm-2 col-md-2">
                         {product?.img ?
                             <img src={product?.img} width='226' height='349' alt='Product' />
                             :
                             <img src={require('./../../Images/BooksImages/Fashion.jpg')} width='226' height='349' alt='Product' />
-
                         }
                     </div>
-                    <div className="mt-4">
+                    <div className="col-4 col-md-4 container">
                         <div className="ml-2">
                             <h2>{product?.title}</h2>
                             <h5 className="text-primary">{product?.seller}</h5>
                             <p className="lead">{product?.description}</p>
                             <StarsReview rating={totalStars} size={32} />
                         </div>
-
                     </div>
-                    <CheckoutAndReviewBox product={product} mobile={false} currentLoansCount={currentLoansCount} />
-                    <hr />
-
-                    <LatestReviews reviews={reviews} productId={product?.id} mobile={true} />
+                    <CheckoutAndReviewBox product={product} mobile={false} currentLoansCount={currentLoansCount}
+                        isAuthenticated={authState?.isAuthenticated} isCheckedOut={isCheckedOut}
+                        checkoutProduct={checkoutProduct} />
                 </div>
+                <hr />
+                <LatestReviews reviews={reviews} productId={product?.id} mobile={true} />
             </div>
-
-        );
-    }
+            <div className="container d-lg-none mt-5">
+                <div className="d-flex justify-content-center align-items-center">
+                    {product?.img ?
+                        <img src={product?.img} width='226' height='349' alt='Product' />
+                        :
+                        <img src={require('./../../Images/BooksImages/Fashion.jpg')} width='226' height='349' alt='Product' />
+                    }
+                </div>
+                <div className="mt-4">
+                    <div className="ml-2">
+                        <h2>{product?.title}</h2>
+                        <h5 className="text-primary">{product?.seller}</h5>
+                        <p className="lead">{product?.description}</p>
+                        <StarsReview rating={totalStars} size={32} />
+                    </div>
+                </div>
+                <CheckoutAndReviewBox product={product} mobile={false} currentLoansCount={currentLoansCount} isAuthenticated={authState?.isAuthenticated} isCheckedOut={isCheckedOut}
+                    checkoutProduct={checkoutProduct} />
+                <hr />
+                <LatestReviews reviews={reviews} productId={product?.id} mobile={true} />
+            </div>
+        </div>
+    );
+}
 
